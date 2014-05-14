0162fb2a6be00495d9843a3a95fb3c2e350e72c8
schang <schang@cs.umn.edu>
2014-05-14 11:33:06 -0500

    Add output file and candidate items options for the recommend cli

--- lenskit-cli/src/main/java/org/grouplens/lenskit/cli/Recommend.java
+++ lenskit-cli/src/main/java/org/grouplens/lenskit/cli/Recommend.java
@@ -21,6 +21,7 @@
 package org.grouplens.lenskit.cli;
 
 import com.google.common.base.Stopwatch;
+import com.google.common.io.Closer;
 import net.sourceforge.argparse4j.impl.Arguments;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.Namespace;
@@ -33,12 +34,16 @@ import org.grouplens.lenskit.util.io.LKFileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 /**
  * Generate Top-N recommendations for users.
@@ -77,20 +82,61 @@ public class Recommend implements Command {
         Symbol pchan = getPrintChannel();
         Stopwatch timer = new Stopwatch();
         timer.start();
-        for (long user: users) {
-            List<ScoredId> recs = irec.recommend(user, n);
-            System.out.format("recommendations for user %d:\n", user);
-            for (ScoredId item: recs) {
-                System.out.format("  %d: %.3f", item.getId(), item.getScore());
-                if (pchan != null && item.hasUnboxedChannel(pchan)) {
-                    System.out.format(" (%f)", item.getUnboxedChannelValue(pchan));
+        File output = options.get("output_file");
+        Set<Long> candidates = getCandidates();
+        if(output != null) {
+            logger.info("writing recommendations to file {}", output);
+            Closer closer = Closer.create();
+            try {
+                OutputStream stream = closer.register(new FileOutputStream(output));
+                if (LKFileUtils.isCompressed(output)) {
+                    stream = closer.register(new GZIPOutputStream(stream));
                 }
-                System.out.println();
+                for (long user: users) {
+                    List<ScoredId> recs;
+                    if(candidates!=null){
+                        recs = irec.recommend(user, n, candidates, null);
+                    } else {
+                        recs = irec.recommend(user, n);
+                    }
+                    System.out.format("recommendations for user %d:\n", user);
+                    for (ScoredId item: recs) {
+                        System.out.format("  %d: %.3f", item.getId(), item.getScore());
+                        String line = String.format("%d, %d, %.3f\n", user, item.getId(), item.getScore());
+                        stream.write(line.getBytes());
+                        if (pchan != null && item.hasUnboxedChannel(pchan)) {
+                            System.out.format(" (%f)", item.getUnboxedChannelValue(pchan));
+                        }
+                        System.out.println();
+                    }
+                }
+            } catch (Throwable th) {
+                throw closer.rethrow(th);
+            } finally {
+                closer.close();
+            }
+        } else {
+            for (long user : users) {
+                List<ScoredId> recs;
+                if(candidates!=null){
+                    recs = irec.recommend(user, n, candidates, null);
+                } else {
+                    recs = irec.recommend(user, n);
+                }
+                System.out.format("recommendations for user %d:\n", user);
+                for (ScoredId item : recs) {
+                    System.out.format("  %d: %.3f", item.getId(), item.getScore());
+                    if (pchan != null && item.hasUnboxedChannel(pchan)) {
+                        System.out.format(" (%f)", item.getUnboxedChannelValue(pchan));
+                    }
+                    System.out.println();
+                }
             }
         }
         timer.stop();
         logger.info("recommended for {} users in {}", users.size(), timer);
     }
+
 
     private LenskitRecommenderEngine loadEngine() throws RecommenderBuildException, IOException {
         File modelFile = options.get("model_file");
@@ -145,6 +191,20 @@ public class Recommend implements Command {
         }
     }
 
+    Set<Long> getCandidates() throws IOException {
+        File f = options.get("candidate");
+        Path file = f.toPath();
+        if(file == null) {
+            return null;
+        }
+        List<String> lines = Files.readAllLines(file, Charset.defaultCharset());
+        Set<Long> candidates = new HashSet<Long>();
+        for(String id : lines) {
+            candidates.add(Long.parseLong(id));
+        }
+        return candidates;
+    }
+
     public static void configureArguments(ArgumentParser parser) {
         InputData.configureArguments(parser);
         ScriptEnvironment.configureArguments(parser);
@@ -158,6 +218,14 @@ public class Recommend implements Command {
               .action(Arguments.append())
               .metavar("FILE")
               .help("use configuration from FILE");
+        parser.addArgument("-o", "--output-file")
+                .type(File.class)
+                .metavar("FILE")
+                .help("the output file of the recommendation results");
+        parser.addArgument("--candidate")
+                .type(File.class)
+                .metavar("FILE")
+                .help("the candidate movie ids for recommendation");
         parser.addArgument("-m", "--model-file")
               .type(File.class)
               .metavar("FILE")
